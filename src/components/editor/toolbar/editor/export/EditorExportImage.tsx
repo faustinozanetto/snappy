cc38bb23ad201a6f243fc2c02537cc8e1f8b49d1
Faustino Zanetto <faustizanetto@gmail.com>
2022-04-24 19:34:52 -0300

    Improved export logic.

--- src/components/editor/toolbar/editor/export/EditorExportImage.tsx
+++ src/components/editor/toolbar/editor/export/EditorExportImage.tsx
@@ -11,21 +11,15 @@ import {
   useColorModeValue,
 } from '@chakra-ui/react';
 import { FaSave } from 'react-icons/fa';
-import { Options as NodeToImageOptions } from 'dom-to-image';
 import { PopoverTrigger } from '../popover/PopoverTrigger';
 import {
   selectExportCustomization,
-  selectFontCustomization,
   FileExtension,
-  selectBackgroundCustomization,
-  BackgroundType,
 } from '@state/slices/toolbar/ToolbarEditorCustomization.slice';
 import { useSelector } from 'react-redux';
 import { EditorExportImaageExtension } from './EditorExportImageExtension';
-import { EXPORT_SIZES_HASH } from '@lib/Constants';
 import { toJpeg, toPng, toSvg } from 'html-to-image';
 import { EditorExportImageSize } from './EditorExportImageSize';
-import domtoimage from '../../../../../lib/DomToImage.js';
 
 interface EditorExportImageProps {
   exportRef: React.RefObject<HTMLDivElement>;
@@ -35,102 +29,61 @@ export const EditorExportImage: React.FC<EditorExportImageProps> = ({
   exportRef,
 }) => {
   const exportCustomization = useSelector(selectExportCustomization);
-  const fontCustomization = useSelector(selectFontCustomization);
-  const backgroundCustomization = useSelector(selectBackgroundCustomization);
 
-  const onButtonClick = useCallback(async () => {
-    if (exportRef.current === null) {
-      return;
-    }
+  const onButtonClick = useCallback(
+    async (
+      extension: FileExtension = FileExtension.PNG,
+      quality: number = 1
+    ) => {
+      if (exportRef.current === null) {
+        return;
+      }
 
-    await toPng(exportRef.current, {})
-      .then((dataUrl) => {
-        const link = document.createElement('a');
-        link.download = 'my-image-name.png';
-        link.href = dataUrl;
-        link.click();
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, [exportRef]);
+      // PNG
+      if (extension === FileExtension.PNG) {
+        await toPng(exportRef.current, { quality })
+          .then((dataUrl) => {
+            const link = document.createElement('a');
+            const NAME = 'snappy';
+            const EXTENSION = 'png';
+            link.download = `${NAME}.${EXTENSION}`;
+            link.href = dataUrl;
+            link.click();
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      }
 
-  const generateImage = async (
-    format: FileExtension,
-    exportSize = EXPORT_SIZES_HASH[exportCustomization.sizeMultiplier]
-  ) => {
-    const REF = exportRef.current;
-    const width = REF.offsetWidth;
-    const height = REF.offsetHeight;
-    exportSize = EXPORT_SIZES_HASH[exportCustomization.sizeMultiplier];
-    console.log({ width, height, exportSize });
-
-    const config: NodeToImageOptions = {
-      width,
-      height,
-      filter: () => {
-        return true;
-      },
-      style: {
-        transform: `scale(${exportSize})`,
-        'transform-origin': 'center',
-        background:
-          backgroundCustomization.backgroundType === BackgroundType.COLOR
-            ? backgroundCustomization.backgroundColor
-            : 'none',
-      },
-    };
-
-    if (format === 'svg') {
-      return domtoimage
-        .toSvg(REF, config)
-        .then((dataURL) =>
-          dataURL
-            .replace(/&nbsp;/g, '&#160;')
-            // https://github.com/tsayen/dom-to-image/blob/fae625bce0970b3a039671ea7f338d05ecb3d0e8/src/dom-to-image.js#L551
-            .replace(/%23/g, '#')
-            .replace(/%0A/g, '\n')
-            // https://stackoverflow.com/questions/7604436/xmlparseentityref-no-name-warnings-while-loading-xml-into-a-php-file
-            .replace(/&(?!#?[a-z0-9]+;)/g, '&amp;')
-            // remove other fonts which are not used
-            .replace(
-              // current font-family used
-              new RegExp(
-                '@font-face\\s+{\\s+font-family: (?!"*' +
-                  fontCustomization.fontFamily +
-                  ').*?}',
-                'g'
-              ),
-              ''
-            )
-        )
-        .then((uri) => uri.slice(uri.indexOf(',') + 1))
-        .then((data) => new Blob([data], { type: 'image/svg+xml' }));
-    }
-
-    // Twitter and Imgur needs regular dataURLs
-    const data = await domtoimage.toPng(REF, config);
-    return data;
-  };
-
-  const exportImage = async (format: FileExtension, options: any = {}) => {
-    const link = document.createElement('a');
-    const prefix = options.filename;
-
-    return await generateImage(format)
-      .then((blob) => {
-        const BLOB = new Blob([blob], { type: 'image/png' });
-        return URL.createObjectURL(BLOB);
-      })
-      .then((url) => {
-        link.download = prefix + '.' + format;
-        link.href = url;
-        //link.target = '_blank';
-        document.body.appendChild(link);
-        link.click();
-        link.remove();
-      });
-  };
+      // SVG
+      if (extension === FileExtension.SVG) {
+        await toSvg(exportRef.current, {
+          quality,
+        }).then((dataUrl) => {
+          const link = document.createElement('a');
+          const NAME = 'snappy';
+          const EXTENSION = 'svg';
+          link.download = `${NAME}.${EXTENSION}`;
+          link.href = dataUrl;
+          link.click();
+        });
+      }
+      // JPEG
+      if (extension === FileExtension.JPEG) {
+        await toJpeg(exportRef.current, {
+          quality,
+        }).then((dataUrl) => {
+          const link = document.createElement('a');
+          const NAME = 'snappy';
+          const EXTENSION = 'jpeg';
+          link.download = `${NAME}.${EXTENSION}`;
+          link.href = dataUrl;
+          link.click();
+        });
+      }
+    },
+    [exportRef]
+  );
 
   return (
     <Popover placement='left-start'>
@@ -160,7 +113,13 @@ export const EditorExportImage: React.FC<EditorExportImageProps> = ({
             <EditorExportImaageExtension />
             {/* Size Multiplier */}
             <EditorExportImageSize />
-            <Button onClick={onButtonClick} colorScheme='blue' width='100%'>
+            <Button
+              onClick={() =>
+                onButtonClick(exportCustomization.fileExtension, 4)
+              }
+              colorScheme='blue'
+              width='100%'
+            >
               Export
             </Button>
           </VStack>
