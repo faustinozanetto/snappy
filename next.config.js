42a451e16a07725d97a5172dfb78ebe5898e2057
Faustino Zanetto <faustizanetto@gmail.com>
2022-05-08 12:54:50 -0300

    Started implementing pwa.

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withPWA = require('next-pwa');
+
 /** @type {import('next').NextConfig} */
-module.exports = {
+module.exports = withPWA({
+  pwa: {
+    dest: 'public',
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === 'development',
+  },
   images: {
     domains: ['https://snappyapp.vercel.app'],
   },
   swcMinify: true,
-};
+});
